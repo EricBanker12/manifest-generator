d2f015771d549f3809ec4b9eb0e7a2c2fb516244
Mister-Kay <private-information>
2018-06-14 07:50:11 -0500

    Allow Drag-n'-Drop launch method

--- manifest-generator.js
+++ manifest-generator.js
@@ -33,11 +33,25 @@ const forceUnixFileTypes = [
     '.lst'
 ]
 
+// set directory to launch argument or local directory
+let directory = __dirname
+if (process.argv[2]) {
+    directory = process.argv[2]
+    // check if valid directory
+    try {
+        fs.readdirSync(directory, 'utf8')
+    }
+    catch (err) {
+        console.log(`"${directory}" is not a valid folder.`)
+        return
+    }
+}
+
 // read existing manifest.json
 let manifest
 try {
     // sanitize input
-    manifest = require('./manifest.json')
+    manifest = require(path.join(directory, 'manifest.json'))
     if (manifest && typeof manifest === 'object') {
         if (!manifest.files) manifest.files = {}
     }
@@ -59,7 +73,7 @@ let checking = 0
 for (let entry of Object.keys(manifest.files)) {
     // check if file exists
     checking  += 1
-    fs.access(path.join(__dirname, entry), fs.constants.F_OK, (err) => {
+    fs.access(path.join(directory, entry), fs.constants.F_OK, (err) => {
         checking -= 1
         if (err) delete manifest.files[entry]
         checkProg()
@@ -72,7 +86,7 @@ getFiles()
 
 // get all files in folder and subfolder
 function getFiles(relativePath = '', files) {
-    let dir = path.join(__dirname, relativePath)
+    let dir = path.join(directory, relativePath)
     if (!files) files = fs.readdirSync(dir, 'utf8')
     for (let file of files) {
         // if not ignored file or begins with . or _
@@ -97,20 +111,27 @@ function getHash(file, type = 'sha256') {
     file = file.replace(/\\/g, '/')
     if (forceUnixLineEndings) forceUnix(file)
     if (manifest.files[file] && typeof manifest.files[file] === 'object') {
-        manifest.files[file].hash = crypto.createHash(type).update(fs.readFileSync(path.join(__dirname, file))).digest('hex')
+        manifest.files[file].hash = crypto.createHash(type).update(fs.readFileSync(path.join(directory, file))).digest('hex')
     }
     else {
-        manifest.files[file] = crypto.createHash(type).update(fs.readFileSync(path.join(__dirname, file))).digest('hex')
+        manifest.files[file] = crypto.createHash(type).update(fs.readFileSync(path.join(directory, file))).digest('hex')
     }
 }
 
 // force unix line endings
 function forceUnix(file) {
+    // check if read and writable
     for (let type of forceUnixFileTypes) {
         if (file.slice(-6).includes(type)) {
-            let data = fs.readFileSync(path.join(__dirname, file), 'utf8')
-            data = data.replace(/\r\n/g, '\n')
-            fs.writeFileSync(path.join(__dirname, file), data, 'utf8')
+            try {
+                let data = fs.readFileSync(path.join(directory, file), 'utf8')
+                data = data.replace(/\r\n/g, '\n')
+                fs.writeFileSync(path.join(directory, file), data, 'utf8')
+            }
+            catch (err) {
+                //console.log(err)
+                console.log('Cannot edit protected file: ' + file)
+            }
             return
         }
     }
@@ -119,7 +140,7 @@ function forceUnix(file) {
 // check if process completed
 function checkProg() {
     if (reading === 0 && checking === 0) {
-        fs.writeFileSync(path.join(__dirname, 'manifest.json'), JSON.stringify(manifest, null, '\t'), 'utf8')
+        fs.writeFileSync(path.join(directory, 'manifest.json'), JSON.stringify(manifest, null, '\t'), 'utf8')
         console.log('"manifest.json" generation complete.')
     }
 }
